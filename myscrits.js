f4f7c210def0cf99e5448434807e274e0b67b83f
Luis Villalon <luisfvilla012@gmail.com>
2022-07-11 11:24:14 -0700

    Wrote a function, playRound(), that takes two parameters and then returns a string that declares the winner of the round. I made the playerSelection parameter case-insensitive.

--- myscrits.js
+++ myscrits.js
@@ -1,6 +1,40 @@
-const Hand = ["rock", "paper", "scissors"];
-function computerPlay (Hand) {
+function computerPlay () {
+    const Hand = ["rock", "paper", "scissors"];
     let choise =  Hand[Math.floor(Math.random() * Hand.length)]
     return choise;
 }
+//console.log(computerPlay()) 
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+function playRound (playerSelection, computerSelection) {
+    const Selection = playerSelection.toLowerCase();
+    if (Selection == "rock") {
+        if (computerSelection == "scissors") {
+            return "You win! Rock beats Scissors";
+        } else if (computerSelection == "paper") {
+            return "You lose! Paper beats Rock";
+        } else {
+            return "Tie!";
+        }
+    }
+    if (Selection == "paper") {
+        if (computerSelection == "rock") {
+            return "You win! Paper beats Rock";
+        } else if (computerSelection == "scissors") {
+            return "You lose! Scissors beats Paper";
+        } else {
+            return "Tie!";
+        }
+    }
+    if (Selection == "scissors") {
+        if (computerSelection == "paper") {
+            return "You win! Scissors beats Paper";
+        } else if (computerSelection == "rock") {
+            return "You lose! Rock beats Scissors";
+        } else {
+            return "Tie!";
+        }
+    }
+}
+//console.log(playRound(playerSelection,computerSelection))
 
